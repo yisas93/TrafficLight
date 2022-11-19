51c68d8a063d9adce135f53e197a418306b1be5b
yisas93 <juan249325@gmail.com>
2022-11-19 23:28:48 +0000

    trafficlight

---
+++ src/js/component/semaforo.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+const Trafficlight = ()=>{
+    const [color, setColor] =useState("")
+    return <div className="container d-flex justify-content-center">
+         <div  className="thing "></div>
+            <div className="TrafficLight">
+            <div onClick={()=>setColor("green")} className={color=="green"?"clicked light green":"light green"}></div>
+            <div onClick={()=>setColor("red")} className={color=="red"?"clicked light red":"light red"}></div>
+            <div onClick={()=>setColor("yellow")} className={color=="yellow"?"clicked light yellow":"light yellow"}></div>
+    </div>
+    </div>
+      
+}
+
+export default Trafficlight
